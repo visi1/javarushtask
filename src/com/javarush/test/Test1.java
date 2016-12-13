685b0e511c36b6554d74fdbc2e0f683e0ba663a6
Gulin Maksim <gulin.maxim.a@gmail.com>
2016-12-13 20:29:45 +0300

    Test1 class add

---
+++ src/com/javarush/test/Test1.java
@@ -0,0 +1,36 @@
+package com.javarush.test;
+
+/**
+ * Created by gulin_ma on 13.12.2016.
+ */
+public class Test1 {
+
+    private int anInt;
+    private String string;
+    private long abls;
+
+    public void setAnInt(int anInt) {
+        this.anInt = anInt;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public void setAbls(long abls) {
+        this.abls = abls;
+    }
+
+    public int getAnInt() {
+
+        return anInt;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public long getAbls() {
+        return abls;
+    }
+}
